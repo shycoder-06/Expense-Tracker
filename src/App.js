a7583140d1f3b33414c810fceecb3f9263507b87
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 18:38:41 -0500

    con tetx folder, and global state created within context to handle transaction states

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ function App() {
       <Balance />
       <IncomeExpenses />
       <TransactionList />
+      <AddTransaction />
      </div>
       
     </div>
