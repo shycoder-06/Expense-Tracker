b0a377e7a1f07e5fca0cae0f00f581f3a3a57122
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 18:49:53 -0500

    appreducer updated , globalstate updated to include global provider

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 function App() {
   return (
-    <div>
-     <Header />
-     <div className='container'>
-      <Balance />
-      <IncomeExpenses />
-      <TransactionList />
-      <AddTransaction />
-     </div>
-      
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+
+    </GlobalProvider>
   );
 }
 
