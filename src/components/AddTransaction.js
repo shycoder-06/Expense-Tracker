8bbb3a83c1022a909584c2bfa27ccc9020c4288e
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 18:26:31 -0500

    added state for form elements (text, amt)

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AddTransaction = () => {
+    const [text, setText]=useState('');
+    const [amount, setAmount]=useState(0);
   return (
     <>
     
@@ -8,14 +10,14 @@ export const AddTransaction = () => {
       <form id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
