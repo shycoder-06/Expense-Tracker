ef6dbb0fd876e3b28519fe35b8ce8e1a0dad07c0
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 22:55:28 -0500

    added actions (addtransaction, delete transactions)

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,33 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
-    const [text, setText]=useState('');
-    const [amount, setAmount]=useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+  const onSubmit = e => {
+    e.preventDefault();
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount
+
+    }
+  }
+
+
   return (
     <>
-    
-    <h3>Add new transaction</h3>
-      <form id="form">
+
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit} id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount"
-            >Amount <br />
+          >Amount <br />
             (negative - expense, positive - income)</label
           >
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount..." />
@@ -22,6 +35,6 @@ export const AddTransaction = () => {
         <button className="btn">Add transaction</button>
       </form>
     </>
-  
+
   )
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -29,9 +29,19 @@ export const GlobalProvider = ({ children }) => {
         });
     }
 
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+
     return (
         <GlobalContext.Provider value={{
-            transactions: state.transactions
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
