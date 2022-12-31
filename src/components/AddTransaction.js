c5c99c5337586dab26c90dc7f859d01f8e0ffeef
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 22:59:05 -0500

    AddTransaction component complete. Final commit

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,9 +10,9 @@ export const AddTransaction = () => {
     const newTransaction = {
       id: Math.floor(Math.random() * 100000000),
       text,
-      amount
-
+      amount: +amount
     }
+    addTransaction(newTransaction)
   }
 
 
