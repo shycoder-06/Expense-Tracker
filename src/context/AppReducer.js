936aa3beb1b987761e57a436221e99bf14ae7789
Shyla <shailajasrinivas91@gmail.com>
2022-12-30 22:40:34 -0500

    updated

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,7 @@
 export default (state, action) => {
     switch (action.type) {
+        case 'DELETE_TRANSACTION':
+            return
         default:
             return state;
     }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,7 +5,7 @@ import AppReducer from './AppReducer';
 const initialState = {
     transactions: [
         { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
+        { id: 2, text: 'Salary', amount: 250 },
         { id: 3, text: 'Book', amount: -10 },
         { id: 4, text: 'Camera', amount: 150 }
     ]
@@ -20,6 +20,15 @@ export const GlobalContext = createContext(initialState)
 
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    //Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions
